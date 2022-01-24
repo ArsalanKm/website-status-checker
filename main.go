9436c2d932f7d8febe4fad0220b3e969f63cc538
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-01-24 23:12:21 +0330

    recieving messages from channel with a trivial way

--- main.go
+++ main.go
@@ -8,23 +8,37 @@ import (
 func main(){
 
 	links := []string{
-		"http://google.com",
-		"http://facebook.com",
-		"http://golang.org",
-		"http://amazon.com",
+		"http://ramzinex.com",
+		"http://digikala.com",
+		"http://nobitex.ir",
+		"http://snapp.ir",
 	}
+
+	c:=make(chan string)
+	
 
 	for _,link:= range links{
-		checkLink(link)
+		go checkLink(link, c)
 	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+
+
+
 }
 
-func checkLink(link string){
+func checkLink(link string,c chan string){
 	_, err := http.Get(link)
+
 	if err !=nil {
 		fmt.Println(link , "might be down !")
+		c <- "Might be down I Think"
 		return 
 	}
 	fmt.Println(link,"is up !")
+	c <- "Yep it is up ! "
 
 }
